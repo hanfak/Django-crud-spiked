8a83aaa4d7071d41a45b4850702956351421dd50
Hanif Fakira <fakira.work@gmail.com>
2016-12-26 13:39:50 +0000

    show route to display single  question and choices

--- polls/views.py
+++ polls/views.py
@@ -1,16 +1,18 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 from .models import Question, Choice
-from django.shortcuts import render
 
 # Create your views here.
 
 def index(request):
-    latest_question_list = Question.objects.order_by('pub_date')[:5]
-    context = {
-        'latest_question_list': latest_question_list,
-    }
+    latest_question_list = Question.objects.all()
+    context = { 'latest_question_list': latest_question_list, }
     return render(request, 'polls/index.html', context)
 
-def detail(request, question_id):
-    return HttpResponse("details %s" % question_id)
+def show(request, question_id):
+    a_question = Question.objects.get(pk = question_id)
+    all_choices = a_question.choice_set.all()
+    context = {
+        'a_question': a_question,
+        'all_choices': all_choices,
+    }
+    return render(request, 'polls/show.html', context)
