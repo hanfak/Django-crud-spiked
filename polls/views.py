2594f2c562dff437e3cc53bf0926cb8e84f6e950
Hanif Fakira <fakira.work@gmail.com>
2016-12-26 11:05:38 +0000

    linking models to controller n url

--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Question, Choice
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("yo")
+    latest_question_list = Question.objects.order_by('pub_date')[:5]
+    output = ', '.join([q.question_text for q in latest_question_list])
+    return HttpResponse(output)
+
+def detail(request, question_id):
+    return HttpResponse("details %s" % question_id)
